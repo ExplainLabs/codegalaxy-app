92edcf4c7e534321ea54ea051d40b8a086647921
Viktor Podzigun <viktor.podzigun@gmail.com>
2021-12-06 11:55:14 +0100

    Extracted QuestionView component (#49)

--- app/src/main/scala/io/codegalaxy/app/question/QuestionScreen.scala
+++ app/src/main/scala/io/codegalaxy/app/question/QuestionScreen.scala
@@ -19,18 +19,13 @@ case class QuestionScreenProps(dispatch: Dispatch,
 
 object QuestionScreen extends FunctionComponent[QuestionScreenProps] {
 
-  private[question] var questionChoicesComp: UiComponent[QuestionChoicesProps] = QuestionChoices
-  private[question] var questionTextComp: UiComponent[QuestionTextProps] = QuestionText
-  private[question] var questionButtonComp: UiComponent[QuestionButtonProps] = QuestionButton
-  private[question] var questionAnswerComp: UiComponent[QuestionAnswerProps] = QuestionAnswer
-  private[question] var questionRuleComp: UiComponent[QuestionRuleProps] = QuestionRule
+  private[question] var questionViewComp: UiComponent[QuestionViewProps] = QuestionView
   
   protected def render(compProps: Props): ReactElement = {
     implicit val theme: Theme = useTheme()
     val props = compProps.wrapped
     val topic = props.params.topic
     val chapter = props.params.getChapter
-    val (selectedIds, setSelectedIds) = useState(Set.empty[Int])
     
     useEffect({ () =>
       if (!props.data.topic.contains(topic) || !props.data.chapter.contains(chapter)) {
@@ -45,48 +40,16 @@ object QuestionScreen extends FunctionComponent[QuestionScreenProps] {
     )(props.data.question match {
       case None => <.Text(^.rnStyle := themeTextStyle)("Loading...")
       case Some(question) =>
-        val multiSelect = question.answerType != "SINGLE_CHOICE"
-        val answered = question.correct.isDefined
-        
         <.ScrollView(^.keyboardShouldPersistTaps := KeyboardShouldPersistTaps.always)(
-          <(questionTextComp())(^.wrapped := QuestionTextProps(question.text))(),
-
-          <(questionChoicesComp())(^.wrapped := new QuestionChoicesProps(
-            answered = answered,
-            choices = question.choices,
-            selectedIds = selectedIds,
-            setSelectedIds = setSelectedIds,
-            multiSelect = multiSelect
-          ))(),
-
-          question.correct.map { correct =>
-            <(questionAnswerComp())(^.wrapped := QuestionAnswerProps(correct))()
-          },
-
-          question.rules.map { rule =>
-            <(questionRuleComp())(^.wrapped := QuestionRuleProps(rule.title, rule.text))()
-          },
-          question.explanation.collect { case explanation if explanation.trim.nonEmpty =>
-            <(questionRuleComp())(^.wrapped := QuestionRuleProps("Explanation", explanation))()
-          },
-
-          if (!answered) {
-            <(questionButtonComp())(^.wrapped := QuestionButtonProps("Continue", onPress = { () =>
-                val data = question.copy(choices = question.choices.map { choice =>
-                  val selected = selectedIds.contains(choice.id)
-                  choice.copy(selected = if (selected) Some(true) else Some(false))
-                })
-                props.dispatch(props.actions.submitAnswer(props.dispatch, topic, chapter, data))
-              }
-            ))()
-          }
-          else {
-            <(questionButtonComp())(^.wrapped := QuestionButtonProps("Next", onPress = { () =>
-                props.dispatch(props.actions.fetchQuestion(props.dispatch, topic, chapter))
-                setSelectedIds(Set.empty)
-              }
-            ))()
-          }
+          <(questionViewComp())(^.wrapped := QuestionViewProps(
+            data = question,
+            onSubmitAnswer = { data =>
+              props.dispatch(props.actions.submitAnswer(props.dispatch, topic, chapter, data))
+            },
+            onNextQuestion = { () =>
+              props.dispatch(props.actions.fetchQuestion(props.dispatch, topic, chapter))
+            }
+          ))()
         )
     })
   }
